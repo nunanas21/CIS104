4b35b0bbddadc2e3dababbbd7b5095e3ac21c36c
nunanas21 <46700814+nunanas21@users.noreply.github.com>
2019-01-29 17:45:45 -0800

    Add files via upload

---
+++ H1P1.py
@@ -0,0 +1,7 @@
+first_name = input("Please enter your first name:")
+last_name = input("Please enter your last name: ")
+age = int(input("Please enter your age: ")
+confidence = float(input("Please express your confidence in programmers 0-100%") /100
+age_dog_years = (age *7)
+print("Hello", first_name, last_name, "nice to meet you.")
+print("You are", age_dog_years, "in dog years.")
---
+++ H1P2.py
@@ -0,0 +1,3 @@
+miles = int(input("Enter total number of miles:"))
+miles_km = int(miles / 1.609)
+print ("You have traveled", miles_km, "kilometers")
